e8e46ddd61e9e5972a25370e83f2e7be8c998964
JohnCS94 <johnscipion@yahoo.com>
2024-05-31 09:51:16 -0400

    allowed hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
      'http://localhost:3000',
      'http://127.0.0.1:3000',
-     'https://brightest-qxb7.vercel.app'
+     'vercel.app',
 ]
 
 ROOT_URLCONF = 'backend.urls'
